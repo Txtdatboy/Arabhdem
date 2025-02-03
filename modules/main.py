fb66e75a786ddf044a0027fe7a6e476a2254a168
Txtdatboy <herokukeliy@outlook.com>
2025-02-03 08:53:14 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -26,7 +26,7 @@ bot = Client(
     "bot",
     api_id=28466214,
     api_hash= "3f55d44aae0f6c72f0dd8855adeeb60f",
-    bot_token= "6838714243:AAGaMHzCLFLx_QPgDOHXkZZz02lFUR67nfI")
+    bot_token= "7232704557:AAEXfIc4Nl01lIDAeY2FWf08DHX7v0LnEUc")
 
 
 @bot.on_message(filters.command(["start"]))
@@ -218,6 +218,18 @@ async def account_login(bot: Client, m: Message):
                         await m.reply_text(str(e))
                         time.sleep(e.x)
                         continue
+                elif "ext=html" or ".html" in url:
+                    try:
+                        cmd = f'yt-dlp -o "{name}.html" "{url}"'
+                        download_cmd = f"{cmd} -R 25 --fragment-retries 25"
+                        os.system(download_cmd)
+                        copy = await bot.send_document(chat_id=m.chat.id, document=f'{name}.html', caption=cc1)
+                        count += 1
+                        os.remove(f'{name}.html')
+                    except FloodWait as e:
+                        await m.reply_text(str(e))
+                        time.sleep(e.x)
+                        continue        
                 else:
                     Show = f"**⥥ Downloading ➤**\n**Name ➤** `{name}\n\nQuality ➤ {raw_text2}` \n\n**Bot By ➤ **@MASOOMGURJARTG"
                
